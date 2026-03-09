d56ca07820371c5bf7f3e000a96fcd5458dfc6c0
Camila Gorelik <kamilalove04@gmail.com>
2026-03-09 12:03:32 +0000

    Add initial content to question1.py files for research on productivity factors

---
+++ src/Bea Brown/question1.py
@@ -0,0 +1,6 @@
+"""
+Are there gender differences in productivity levels among students, and if so, what factors contribute to these differences?
+
+Does attendance affect performance, and if so, to what extent?
+
+"""
---
+++ src/Camila Gorelik/question1.py
@@ -0,0 +1,6 @@
+"""
+Does exercise or sleep have a stronger correlation with productivity levels among students?
+
+How do study hours relate to the final grade of students, and is there an optimal number of study hours for maximizing performance?
+
+"""
